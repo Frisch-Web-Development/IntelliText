39466cd4d3f0262c24997ad1cf3b5b8eba52c4ee
Gavri Kepets <gkepets@gmail.com>
2017-09-08 10:19:15 -0400

    Started API

--- MongoAPI/src/main/java/com/mongoapi/persistor/PersistorDao.java
+++ MongoAPI/src/main/java/com/mongoapi/persistor/PersistorDao.java
@@ -1,5 +1,10 @@
 package com.mongoapi.persistor;
 
+import com.main.entity.LoginEntity;
+import com.main.entity.LoginEntityImpl;
+
 public interface PersistorDao {
 
+	void insertNewUser(LoginEntity login, String collection);	
+	
 }
--- MongoAPI/src/main/java/com/mongoapi/persistor/PersistorDaoImpl.java
+++ MongoAPI/src/main/java/com/mongoapi/persistor/PersistorDaoImpl.java
@@ -1,5 +1,19 @@
 package com.mongoapi.persistor;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.main.entity.LoginEntity;
+
 public class PersistorDaoImpl implements PersistorDao{
+
+	@Autowired
+	MongoTemplate mongo;
+	
+	@Override
+	public void insertNewUser(LoginEntity login, String collection) {
+		// TODO Auto-generated method stub
+		mongo.insert(login, collection);
+	}
 
 }
