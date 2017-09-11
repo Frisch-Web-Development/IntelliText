e383ad5639771fb304caf0936ae30ff8d9f283c0
GIP2000 <gpress2222@gmail.com>
2017-09-11 12:06:05 -0400

    Add files via upload
    Sent the googleUser info to the server

--- complete/src/main/java/com/main/MainController.java
+++ complete/src/main/java/com/main/MainController.java
@@ -1,9 +1,16 @@
 package com.main;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -14,13 +21,23 @@ public class MainController {
 	}
 
 	@GetMapping("/login")
-	public String postHome(Model model) {
+	public String getLogin(Model model) {
 		return "login";
 	}
 
 	@GetMapping("/home")
 	public String getHome(Model model) {
 		return "home";
+	}
+
+	
+	
+	
+	@RequestMapping(value = "/login/finish", method = RequestMethod.POST)
+	public ResponseEntity requestHandeler(@RequestBody String data ) {
+		System.out.println(data); 
+		//logic
+		return new ResponseEntity("All good", HttpStatus.OK);
 	}
 
 }
