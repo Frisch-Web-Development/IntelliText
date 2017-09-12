59a2b83007402cc262eb80c1d98dacf00335ae78
GIP2000 <gpress2222@gmail.com>
2017-09-11 21:32:49 -0400

    Login logic
    Login logic works (still need to test it with The API

---
+++ complete/src/main/java/JSON/JSONinterface.java
@@ -0,0 +1,7 @@
+package JSON;
+
+public interface JSONinterface {
+
+	String getArrayValue(int index, String input);
+
+}
--- complete/src/main/java/com/main/MainController.java
+++ complete/src/main/java/com/main/MainController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import JSON.JSONtrans;
+
 @Controller
 public class MainController {
 
@@ -34,10 +36,22 @@ public class MainController {
 	
 	
 	@RequestMapping(value = "/login/finish", method = RequestMethod.POST)
-	public ResponseEntity requestHandeler(@RequestBody String data ) {
+	public ResponseEntity<String> loginHandeler(@RequestBody String data ) {
 		System.out.println(data); 
 		//logic
-		return new ResponseEntity("All good", HttpStatus.OK);
+		String name = JSONtrans.cleanup(JSONtrans.getArrayValue(0,data)); 
+		String email = JSONtrans.cleanup(JSONtrans.getArrayValue(1, data)); 
+		System.out.println(email);
+		System.out.println(name); 
+		if(mongoAPI.doesUserExit())
+		{
+			// Go to users storage page`	
+		}
+		else
+		{
+			mongoAPI.insertUser(name,email); 
+		}
+		return new ResponseEntity<String>("All good", HttpStatus.OK);
 	}
 
 }
