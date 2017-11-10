59afeba3f81fe1f07e1d69f92efea69bf30f5e8a
Gavri Kepets <gkepets@gmail.com>
2017-11-10 15:06:02 -0500

    Sync

--- WebSite/src/main/java/com/controller/MainController.java
+++
@@ -1,9 +0,0 @@
-package com.controller;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-
-//@RestController
-public class MainController {
-
-}
--- WebSite/src/main/java/com/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.model;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
-}
