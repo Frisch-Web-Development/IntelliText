ef81c45c2cb4e9a7a29d2c24a15a4c5e6e91f2d0
gkgkgkgk <gkepets@gmail.com>
2017-11-20 20:56:15 -0500

    Progress

--- WebSite/src/main/java/com/intellitext/controller/MainController.java
+++ WebSite/src/main/java/com/intellitext/controller/MainController.java
@@ -1,9 +1,7 @@
 package com.intellitext.controller;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class MainController {
--- WebSite/src/main/webapp/storage.js
+++ WebSite/src/main/webapp/storage.js
@@ -1,4 +1,4 @@
-$(documnet).ready(function(){
+/*$(document).ready(function(){
 	
 	var user; 
 	var recentFiles;
@@ -71,5 +71,32 @@ $(documnet).ready(function(){
 		
 	}); 
 	
-	
-}); 
+});
+*/
+function onSignIn(googleUser) {
+	console.log("Success");
+
+	var profile = googleUser.getBasicProfile();
+	google = googleUser;
+	var user = {
+		"email" : profile.getEmail(),
+		"firstName" : profile.getGivenName(),
+		"lastName" : profile.getFamilyName(),
+		"socialId" : profile.getId(),
+		"tokenId" : googleUser.getAuthResponse().id_token
+	};
+
+	console.log(user);
+
+	$.ajax({
+		type : 'POST',
+		url : "/conf/user",
+		data : JSON.stringify(user),
+		error : function(e) {
+			console.log(e);
+		},
+		dataType : "json",
+		contentType : "application/json"
+	});
+
+}
